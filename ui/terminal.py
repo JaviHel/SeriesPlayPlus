130e465767ca470f5dec1cbb4322ce00711d15d8
javihel <260128552+JaviHel@users.noreply.github.com>
2026-09-18 20:15:32 -0300

    CAMBIO: renombrar funciones en espanhol

--- ui/terminal.py
+++ ui/terminal.py
@@ -4,11 +4,11 @@ from ascii_frame import *
 
 
 
-# MAIN SETUP
-DATA = cargar_json("datos/dataset_100.json")# IMPLEMENTAR PATHLIB
+# CONFIGURACION PRINCIPAL
+DATOS = cargar_json("datos/dataset_100.json")
 LOOP = True
-WIDTH = 60
-af = AsciiFrame(WIDTH)
+ANCHO_RECUADRO = 60
+af = AsciiFrame(ANCHO_RECUADRO)
 
 
 
@@ -34,14 +34,17 @@ af = AsciiFrame(WIDTH)
 
 
 # FUNCIONES DE IMPRESION EN PANTALLA
-def print_wrapped_text(text, indent="c", separator=" "):
+def imprimir_texto_ensanguchado(text, indent="c", separator=" "):
     """ Imprime Texto Dentro De Una Caja ASCII """
     af.print_box("t")
     af.print_text(text, indent, separator)
     af.print_box("b")
 
 
-def print_wrapped_screen(lst):
+def imprimir_pantalla_ensanguchada(lst):
+    """ Imprime una lista con "cadena de caracteres" en pantalla
+        lst es una lista con cadena de caracteres ["A", "e", "I", "o", "U"]
+    """
     af.print_box("t")
     for i, option in enumerate(lst):
         af.print_text(f"[{i+1}]>" + option)
@@ -52,34 +55,35 @@ def print_wrapped_screen(lst):
 
 
 # FUNCIONES UTILES PARA EL RESTO DE OPCIONES
-def filter_by(user_input:str, option:str):
+def filtrar_por(user_input:str, option:str):
     """ Filtra series por una opcion de manera lineal:
         title, genre, duration, etc...
     """
-    for serie in DATA["series"]:
+    for serie in DATOS["series"]:
         if user_input.lower() == serie[option].lower():
             return True
     return False
 
 
-def select_by(user_input:str, option:str):
+def seleccionar_por(user_input:str, option:str):
     """" Retorna una lista con los nombres de series que tienen una opcion en comun. """
     lst = []
-    for serie in DATA["series"]:
+    for serie in DATOS["series"]:
         if user_input.lower() == serie[option].lower():
             lst.append(serie)
     return lst
     
     
-def get_random_option(option="title"): 
-    """ Retorna una serie aleatoria de a la lista
-        option es el tipo de dato aleatorio que quermos que retorne
+def obtener_opcion_aleatoria(option="title"): 
+    """ Retorna una opcion aleatoria de a la lista json
+        option es la opcion que queremos obtener ("titulo, genero, popularidad")
         "title" por default
     """
-    return random.choice(DATA["series"])[option]
+    return random.choice(DATOS["series"])[option]
     
     
-def exit():
+def salir():
+    """ Rompe el while loop para cerrar el programa """
     global LOOP
     LOOP = False
 
@@ -91,95 +95,95 @@ def exit():
 
 # IMPRIMIR FUNCIONALIDAD DE LAS OPCIONES DE LA PANTALLA PRINCIPAL
 #1
-def search_series():
+def buscar_series():
     """ Imprime si la serie ingresada por el usuario esta en la base de datos """
     # HAY QUE DARLE ESTILO A ESTO
-    name = ask_user("INGRESE EL NOMBRE DE LA SERIE (en ingles): ")
+    nombre = preguntar_usuario("INGRESE EL NOMBRE DE LA SERIE (en ingles): ")
     
 #    print("buscando serie... ")
 
-    if filter_by(name, "title"):
-        print_wrapped_text(f'La serie "{name}" SI se encuentra disponible.')
+    if filtrar_por(nombre, "title"):
+        imprimir_texto_ensanguchado(f'La serie "{nombre}" SI se encuentra disponible.')
     else:
-        print_wrapped_text(f'La serie "{name}" NO se encuentra disponible.')
+        imprimir_texto_ensanguchado(f'La serie "{nombre}" NO se encuentra disponible.')
 
-    exit()
+    salir()
 
 
 
 #2
-def recommend_related():
+def recomendar_relacionado():
     """ Recomienda una serie relacionada a la serie ingresada por el usuario """
-    name = ask_user("INGRESE EL NOMBRE UNA SERIE QUE LE GUSTE (en ingles): ")
+    nombre = preguntar_usuario("INGRESE EL NOMBRE UNA SERIE QUE LE GUSTE (en ingles): ")
     print("NO IMPLEMENTADO AUN :(")
-    exit()
+    salir()
 
 
 
 #3
-def recommend_random():
-    name = get_random_option()
+def recomendar_aleatorio():
+    nombre = obtener_opcion_aleatoria()
     print("recomendando serie aleatoria...")
     
     af.print_box("t")
     af.print_text('TE RECOMIENDO QUE MIRES:', "c")
-    af.print_text(f'"{name}"', "c")
+    af.print_text(f'"{nombre}"', "c")
     af.print_text('¡ESTA MUY BUENA!', "c")
     af.print_box("b")
-    exit()
+    salir()
     
     
     
 #4
-def filter_by_genre():
+def filtrar_por_genero():
     """ Imprime las series del genero ingresado por el usuario """
     print('Ejemplo de generos: "Talk", "Horror", "Comedy", "Crime", etc...')
         
-    genre = ask_user("INGRESE EL NOMBRE DEL GENERO (en ingles): ")
-    lst = select_by(genre, "genre")
+    genero = preguntar_usuario("INGRESE EL NOMBRE DEL GENERO (en ingles): ")
+    lst = seleccionar_por(genero, "genre")
     
 #    print("filtrando por generos...")
     
     if len(lst) > 0:
         af.print_box("t")
-        af.print_text(f'Se encontraron estas series del genero "{genre}"'.upper(), "c",)
+        af.print_text(f'Se encontraron estas series del genero "{genero}"'.upper(), "c",)
         af.print_space("-")
         [af.print_text(f"[{i+1}]>" + serie["title"]) for i, serie in enumerate(lst)]
         af.print_box("b")
     else:
-        print_wrapped_text(f'No se encontro el genero "{genre}"'.upper())
+        imprimir_texto_ensanguchado(f'No se encontro el genero "{genero}"'.upper())
     
-    exit()
+    salir()
 
 
 
 #5
-def filter_by_seasons():
+def filtrar_por_temporadas():
     """ Imprime las series que tienen menos o igual cantidad de temporadas
         que la ingresada por el usuario
     """
     print("NO IMPLEMENTADO AUN :(")
-    exit()
+    salir()
 
 
 
 #6
-def filter_by_episode_duration():
+def filtrar_por_duracion_episodio():
     """ Imprime las series con menos duracion promedio de episodio
         el usuario debe ingresar la duracion promedio de una lista en pantalla
     """
     print("NO IMPLEMENTADO AUN :(")
-    exit()
+    salir()
 
 
 
 #7
-def filter_by_age_rating():
+def filtrar_por_edad():
     """ Imprime las peliculas que son aptas para cierto publico
         el usuario ingresa la opcion de edad de una lista en pantalla
     """
     print("NO IMPLEMENTADO AUN :(")
-    exit()
+    salir()
 
 
 
@@ -187,7 +191,7 @@ def filter_by_age_rating():
 
 
 # FUNCIONES DE INPUT Y SELECCION
-def ask_user(message="text", type_str=True):
+def preguntar_usuario(message="text", type_str=True):
     """ Imprime el mensaje en pantalla para que el usuario vea
         
     """
@@ -196,7 +200,7 @@ def ask_user(message="text", type_str=True):
     return int(input(message))
 
 
-def select_option(user_input:int, method_call_lst:list):
+def seleccionar_opcion(user_input:int, method_call_lst:list):
     """ Dependiendo de lo que el usuario ingrese va a llamar a la opcion correcta 
         method_call_lst es una lista con funciones(sin parentesis) que seran ejecutadas aqui
         segun corresponda.
@@ -223,33 +227,33 @@ def select_option(user_input:int, method_call_lst:list):
 def main():
     # Estas variables y titulos deberian ir cada una dentro de una funcion
     # Variables de los titulos y opciones de cada pantalla
-    MAIN_SCREEN_MESSAGE = "SELECCIONE UNA OPCION DEL MENU: " # mensaje al usuario #1
-    MAIN_SCREEN_TITLE = "🎞 SERIESPLAY 🎞"
+    MENSAJE_PANTALLA_PRINCIPAL = "SELECCIONE UNA OPCION DEL MENU: " # mensaje al usuario #1
+    TITULO_PANTALLA_PRINCIPAL = "🎞 SERIESPLAY 🎞"
     # Creamos un diccionario que lo guardamos en una variable con su respectivo NOMBRE_OPTIONS
     # en el cual la "clave" es lo que se imprime en pantalla
     # y el "valor" es la funcion a llamar(sin parentesis, solo el nombre de la funcion)
-    MAIN_SCREEN_OPTIONS = {"Buscar Series": search_series,
-                           "Recomendar Serie Relacionada": recommend_related, # Implementar mas adelante
-                           "Recomendar Serie Aleatoria": recommend_random,
-                           "Filtrar Por Genero": filter_by_genre,
-                           "Filtrar Por Cantidad De Temporadas": filter_by_seasons,
-                           "Filtrar Por Duracion De Capitulo": filter_by_episode_duration,
-                           "Filtrar Por Edad": filter_by_age_rating,
-                           "Salir":exit,
+    OPCIONES_PANTALLA_PRINCIPAL = {"Buscar Series": buscar_series,
+                                   "Recomendar Serie Relacionada": recomendar_relacionado, # Implementar mas adelante
+                                   "Recomendar Serie Aleatoria": recomendar_aleatorio,
+                                   "Filtrar Por Genero": filtrar_por_genero,
+                                   "Filtrar Por Cantidad De Temporadas": filtrar_por_genero,
+                                   "Filtrar Por Duracion De Capitulo": filtrar_por_duracion_episodio,
+                                   "Filtrar Por Edad": filtrar_por_edad,
+                                   "Salir":salir,
     }       
     
     # Imprime los titulos y las opciones en pantalla
-    print_wrapped_text(MAIN_SCREEN_TITLE) # Imprime el titulo de la pantalla principal 
-    print_wrapped_screen(list(MAIN_SCREEN_OPTIONS.keys())) # convierte en "list/lista" las "keys/llaves" del diccionario
+    imprimir_texto_ensanguchado(MENSAJE_PANTALLA_PRINCIPAL) # Imprime el titulo de la pantalla principal 
+    imprimir_pantalla_ensanguchada(list(OPCIONES_PANTALLA_PRINCIPAL.keys())) # convierte en "list/lista" las "keys/llaves" del diccionario
 
     
     # Loop de la funcionalidad basica
     while LOOP:
         # Los mensajes deberian ser dinamicos tambien
         # Si el "user" no es "int" dejar un mensaje y que no se rompa
-        user = ask_user(MAIN_SCREEN_MESSAGE, False)
+        user = preguntar_usuario(MENSAJE_PANTALLA_PRINCIPAL, False)
         #Necesita el user_input y los valores de un diccionario con funciones
-        select_option(user, list(MAIN_SCREEN_OPTIONS.values()))
+        seleccionar_opcion(user, list(OPCIONES_PANTALLA_PRINCIPAL.values()))
         
     
     
